eb9b877f477f422d3b38bee8dce2e14b5f6c0455
Yunlei Zhang <yunleizhang@Yunlei.local>
2017-05-02 13:48:43 -0400

    convert images in a folder to tfrecord file

---
+++ imageToTFrecord.py
@@ -0,0 +1,177 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import argparse
+import os
+import sys
+
+import tensorflow as tf
+
+from tensorflow.contrib.learn.python.learn.datasets import mnist
+
+FLAGS = None
+
+
+def _int64_feature(value):
+    return tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))
+
+
+def _bytes_feature(value):
+    return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
+
+class ImageCoder(object):
+  """Helper class that provides TensorFlow image coding utilities."""
+
+  def __init__(self):
+    # Create a single Session to run all image coding calls.
+    self._sess = tf.Session()
+
+    # Initializes function that converts PNG to JPEG data.
+    self._png_data = tf.placeholder(dtype=tf.string)
+    image = tf.image.decode_png(self._png_data, channels=3)
+    self._png_to_jpeg = tf.image.encode_jpeg(image, format='rgb', quality=100)
+
+    # Initializes function that decodes RGB JPEG data.
+    self._decode_jpeg_data = tf.placeholder(dtype=tf.string)
+    self._decode_jpeg = tf.image.decode_jpeg(self._decode_jpeg_data, channels=3)
+
+  def png_to_jpeg(self, image_data):
+    return self._sess.run(self._png_to_jpeg,
+                          feed_dict={self._png_data: image_data})
+
+  def decode_jpeg(self, image_data):
+    image = self._sess.run(self._decode_jpeg,
+                           feed_dict={self._decode_jpeg_data: image_data})
+    assert len(image.shape) == 3
+    assert image.shape[2] == 3
+    return image
+
+
+def convert_to(image, label, name):
+    """Converts a dataset to tfrecords."""
+    rows = image.shape[1]
+    cols = image.shape[2]
+    depth = image.shape[3]
+
+    filename = os.path.join(FLAGS.directory, name + '.tfrecords')
+    print('Writing', filename)
+    writer = tf.python_io.TFRecordWriter(filename)
+    image_raw = image.tostring()
+    example = tf.train.Example(features=tf.train.Features(feature={
+        'height': _int64_feature(rows),
+        'width': _int64_feature(cols),
+        'depth': _int64_feature(depth),
+        'label': _bytes_feature(label),
+        'image_raw': _bytes_feature(image_raw)}))
+    writer.write(example.SerializeToString())
+    writer.close()
+
+
+def _convert_to_example(image_buffer, label, text, height, width):
+  """Build an Example proto for an example.
+
+  Args:
+    image_buffer: string, JPEG encoding of RGB image
+    label: integer, identifier for the ground truth for the network
+    text: string, unique human-readable, e.g. 'dog'
+    height: integer, image height in pixels
+    width: integer, image width in pixels
+  Returns:
+    Example proto
+  """
+
+  colorspace = 'RGB'
+  channels = 3
+  image_format = 'JPEG'
+
+  example = tf.train.Example(features=tf.train.Features(feature={
+      'image/height': _int64_feature(height),
+      'image/width': _int64_feature(width),
+      'image/colorspace': _bytes_feature(tf.compat.as_bytes(colorspace)),
+      'image/channels': _int64_feature(channels),
+      'image/class/label': _bytes_feature(tf.compat.as_bytes(label)),
+      'image/class/text': _bytes_feature(tf.compat.as_bytes(text)),
+      'image/format': _bytes_feature(tf.compat.as_bytes(image_format)),
+      'image/encoded': _bytes_feature(tf.compat.as_bytes(image_buffer))}))
+  return example
+
+
+def _process_image(filename, coder):
+  """Process a single image file.
+
+  Args:
+    filename: string, path to an image file e.g., '/path/to/example.JPG'.
+    coder: instance of ImageCoder to provide TensorFlow image coding utils.
+  Returns:
+    image_buffer: string, JPEG encoding of RGB image.
+    height: integer, image height in pixels.
+    width: integer, image width in pixels.
+  """
+  # Read the image file.
+  with tf.gfile.FastGFile(filename, 'rb') as f:
+    image_data = f.read()
+
+  # Convert any PNG to JPEG's for consistency.
+  if _is_png(filename):
+    print('Converting PNG to JPEG for %s' % filename)
+    image_data = coder.png_to_jpeg(image_data)
+
+  # Decode the RGB JPEG.
+  image = coder.decode_jpeg(image_data)
+
+  # Check that image converted to RGB
+  assert len(image.shape) == 3
+  height = image.shape[0]
+  width = image.shape[1]
+  assert image.shape[2] == 3
+
+  return image_data, height, width
+
+
+def _is_png(filename):
+  """Determine if a file contains a PNG format image.
+
+  Args:
+    filename: string, path of the image file.
+
+  Returns:
+    boolean indicating if the image is a PNG.
+  """
+  return '.png' in filename
+
+def main(unused_argv):
+    # Get the data.
+    filename = os.path.join(FLAGS.dest, 'test' + '.tfrecords')
+    writer = tf.python_io.TFRecordWriter(filename)
+    for (dirpath, dirnames, filenames) in os.walk(FLAGS.base, topdown=False):
+        label = dirpath.split("/")[-1]
+        print('label: ' + label)
+        for filename in filenames:
+            if not filename.endswith(('.jpg', '.png')):
+                continue
+            print('image: '+ filename)
+            imagedata, height, width = _process_image(dirpath+'/'+filename, ImageCoder())
+            example = _convert_to_example(imagedata,label, label, height, width)
+            writer.write(example.SerializeToString())
+
+    writer.close()
+    print("Convert images in " + FLAGS.base + " ... Done.")
+
+if __name__ == '__main__':
+  parser = argparse.ArgumentParser()
+  parser.add_argument(
+      '--dest',
+      type=str,
+      default='./tfrecord/',
+      help='Directory to download data files and write the converted result'
+  )
+  parser.add_argument(
+      '--base',
+      type=str,
+      default="./images/",
+      help='Home folder of images.'
+
+  )
+  FLAGS, unparsed = parser.parse_known_args()
+  tf.app.run(main=main, argv=[sys.argv[0]] + unparsed)
